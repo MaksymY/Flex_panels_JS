bccba3e65e84284ab4783ffdd3507b5f124340fd
MaksymY <maksym.yankivskyypro@gmail.com>
2019-02-09 23:56:58 +0100

    add js

--- main.js
+++ main.js
@@ -3,3 +3,13 @@ const panels = document.querySelectorAll('.panels');
 function toggleOpen(){
     this.classList.toggle('open');
 }
+
+function toggleOpen(){
+    if(e.propertyName.includes('flex')){
+        this.classList.toggle('open-active')
+    }
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend',
+   toggleActive));
